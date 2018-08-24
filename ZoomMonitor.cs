25ac58b95f56cf1fd2152f15185d3c97ff5d69f3
bonzaiferroni <lukas8@gmail.com>
2018-08-24 14:12:01 -0600

    implement class zoom

---
+++ ZoomMonitor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace InputUtils
+{
+    public class ZoomMonitor : MonoBehaviour
+    {
+        private List<RaycastResult> _results = new List<RaycastResult>();
+
+        private void Update()
+        {
+            CheckInput();
+        }
+
+        private void CheckInput()
+        {
+            var delta = Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime;
+            if (delta == 0) return;
+            var reciever = FindReceiver();
+            if (reciever == null) return;
+            reciever.Zoom(delta);
+        }
+
+        private ZoomReceiver FindReceiver()
+        {
+            PointerEventData ped = new PointerEventData(null);
+            ped.position = Input.mousePosition;
+            _results.Clear();
+            EventSystem.current.RaycastAll(ped, _results);
+            foreach (var result in _results)
+            {
+                var receiver = result.gameObject.GetComponent<ZoomReceiver>();
+                if (receiver) return receiver;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ ZoomReceiver.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace InputUtils
+{
+    public class ZoomReceiver : MonoBehaviour
+    {
+        public float Speed { get; private set; } = 10;
+        public Transform Target;
+        
+        public void Zoom(float delta)
+        {
+            Target.localScale *= (1 + delta * Speed);
+        }
+    }
+}
